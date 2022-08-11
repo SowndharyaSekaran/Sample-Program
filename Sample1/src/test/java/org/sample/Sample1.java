b6839238032b9eb9f8094935183f43ee09c62ee9
SowndharyaSekaran <ssowndharyact@gmail.com>
2022-08-11 22:42:33 +0530

    Quote added

--- Sample1/src/test/java/org/sample/Sample1.java
+++ Sample1/src/test/java/org/sample/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("Hi Sowndharya,");
 		System.out.println("Welcome to our motivational quotes");
+		System.out.println("Do well never give up");
 	}
 
 }
