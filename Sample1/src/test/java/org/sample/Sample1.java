bbd232be39b6c724bc91eda8f4af68d2594bdeb9
SowndharyaSekaran <ssowndharyact@gmail.com>
2022-08-11 22:39:25 +0530

    Added with motivational quotes

--- Sample1/src/test/java/org/sample/Sample1.java
+++ Sample1/src/test/java/org/sample/Sample1.java
@@ -3,8 +3,8 @@ package org.sample;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, Welcome");
-		System.out.println("Hi Sowndharya, Welcome");
+		System.out.println("Hi Sowndharya,");
+		System.out.println("Welcome to our motivational quotes");
 	}
 
 }
